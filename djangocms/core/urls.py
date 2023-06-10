e7682f8e94394e30e8bbe07fdf6f96794bc47bf9
alicjarogowiec <rogowiec.alicja@gmail.com>
2023-06-10 13:33:45 +0200

    projekt i resources

---
+++ djangocms/core/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('post-detail/<int:pk>', views.PostDetail, name='post_detail'),
+]
